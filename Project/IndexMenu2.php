951b5e2d9b99519ca8c1ab7367eb2e891ca11c0c
118306053 <118306053@BISBIS-U48UJOJN.bis.ad.ucc.ie>
2019-03-07 13:53:13 +0000

    meow

---
+++ Project/IndexMenu2.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Menu</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/SportCss.css"/>
+         <style>
+            body {
+                background-image: url("cloudmenu.jpeg");    
+            }
+           
+           .button { background-color: lightblue;
+                              border: none;
+                              
+                              padding: 20px 40px;
+                              text-align: center;
+                              color: black;
+                              text-decoration: none;
+                              display: inline-block;
+                              font-size: 16px;
+                              margin: 10px 5px;
+                              cursor: pointer;                     
+                    }
+            </style>
+        
+        
+    </head>
+    <body>
+        <div>
+            <h1> <b> Menu </b></h1>
+            
+            <center>
+                <a href="Interests.html" class="button">Interests</a>
+                <br></br>
+                 <a href="CV.html" class="button">Curriculum Vitae</a>
+                <br></br>
+                <a href="ConsultingService.html" class="button">Consultancy Services</a>
+                <br></br>
+                  <a href="https://project118306053.herokuapp.com/"> HEROKU LINK</a>
+                  <p> Student Number: 118306053</p>
+            </center>
+            </div>
+    </body>
+</html>
+
+
